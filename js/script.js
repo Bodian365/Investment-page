9a698bd46207fe1d8694510d7289b8683a80cd64
BohdanIvanyna <ivanyna.bohdan@student.uzhnu.edu.ua>
2025-09-12 23:16:35 +0300

    final version (without sticky nav)

--- js/script.js
+++ js/script.js
@@ -8,3 +8,15 @@ btnMenu.addEventListener("click", function () {
 
 const year = new Date().getFullYear();
 copyright.textContent = year;
+
+///////////////////////////////
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    if (link.classList.contains("main-list-item")) {
+      headerEl.classList.remove("nav-open");
+    }
+  });
+});
